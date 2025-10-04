1dac0df039ef048e5d023867553cc82f0a0011db
KingVishal703 <vishal21wankhede@gmail.com>
2025-10-04 19:53:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -193,7 +193,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'like-andrea-moviesrenamebit-3b73ba9e.koyeb.app'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'identical-eva-vibro1-bd7bf5fd.koyeb.app'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
